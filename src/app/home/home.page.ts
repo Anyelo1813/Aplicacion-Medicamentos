752891dbae7cd06f9577669be9425643183b4faa
yace2206 <2018-0304@uteco.edu.do>
2024-11-30 13:46:34 -0400

    Aprecen las notificaciones.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton,IonIcon,IonButtons, IonLabel, IonMenuButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, IonLabel, IonMenuButton } from '@ionic/angular/standalone';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 
 @Component({
@@ -8,11 +9,25 @@ import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton,IonIcon,IonButton
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton,IonIcon,IonButtons,IonLabel,IonMenuButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, IonLabel, IonMenuButton],
 })
-export class HomePage {
-  constructor() {}
-  onClick(){
+export class HomePage implements OnInit {
+  constructor() { }
+  onClick() {
 
   }
+  ngOnInit(): void {
+    console.log("Iniciando");
+    LocalNotifications.schedule({
+      notifications: [
+        {
+          title: "Título de la notificación",
+          body: "Este es el cuerpo de la notificación.",
+          id: 1,
+          schedule: { at: new Date(new Date().getTime() + 1000 * 5) }, // Notificación después de 5 segundos
+        }]
+    });
+  }
+
+
 }
